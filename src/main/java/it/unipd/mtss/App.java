7674e43337e736a9bc964f92fe08bfa98e6420c6
mattiacadamuro <mattia.cadamuro04@gmail.com>
2025-05-01 11:27:12 +0200

    test app

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////
+// MATTIA CADAMURO 2111534
+// MICHELE TESSER 2111012
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void checkResult(String input) {
+        // Todo
+    }
+}
