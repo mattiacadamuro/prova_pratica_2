9f8b1ebc87f4222214249b50238d0dd5401dfd83
mattiacadamuro <mattia.cadamuro04@gmail.com>
2025-05-01 10:34:41 +0200

    test versione 1.0

---
+++ src/test/java/it/unipd/mtss/RomanNumberTest.java
@@ -0,0 +1,11 @@
+package it.unipd.mtss;
+
+import org.junit.Before;
+
+public class RomanNumberTest {
+    RomanNumber romanNumber;
+    @Before
+    public void setUp() {
+        romanNumber = new RomanNumber();
+    }    
+}
