b016b15352b923ba84890454f36bf2fd9f4fe205
mattiacadamuro <mattia.cadamuro04@gmail.com>
2025-05-01 11:29:15 +0200

    codice app(close #12)

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -5,18 +5,31 @@
 
 package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("*** Welcome to the Roman Number Printer ***");
+        if (args == null || args.length == 0) {
+            System.out.println("Usage: java -jar RomanNumberPrinter.jar <number1> <number2> ...");
+            return;
+        }
+        for (String s : args) {
+            System.out.println("Result for " + s + " is:");
+            checkResult(s);
+        }
+
     }
 
     public static void checkResult(String input) {
-        // Todo
+        try {
+            String result = RomanPrinter.print(Integer.parseInt(input));
+            if (result == "") {
+                System.err.println("There is a error for " + input + ", please input again.");
+            } else {
+                System.out.println(result);
+            }
+        } catch (Exception e) {
+            System.err.println(input + " is not a valid number.");
+        }
     }
+
 }
