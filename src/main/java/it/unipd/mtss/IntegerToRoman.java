12cefe40612549850ed7fe8efacf4bbcddc04f13
Michele <mites0202@gmail.com>
2025-05-01 10:15:55 +0200

    Integer to Roman CDM (close #9)

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,8 +6,8 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
-    private static final int[] values = {50, 40, 10, 9, 5, 4, 1};
-    private static final String[] symbols = {"L", "XL", "X", "IX", "V", "IV", "I"};
+    private static final int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     public static String convert(int number) {
         StringBuilder roman = new StringBuilder();
