606515d9f367bb3465d719dc68de5a7c18c52ff7
mattiacadamuro <mattia.cadamuro04@gmail.com>
2025-05-01 09:58:36 +0200

    Integer to Roman IVXL(close #8)

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,10 +6,17 @@
 package it.unipd.mtss;
 
 public class IntegerToRoman {
+    private static final int[] values = {50, 40, 10, 9, 5, 4, 1};
+    private static final String[] symbols = {"L", "XL", "X", "IX", "V", "IV", "I"};
 
-    public static String convert(int number){
-        //TODO
-        return null;
+    public static String convert(int number) {
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < values.length && number >= 0; i++) {
+            while (values[i] <= number) {
+                number -= values[i];
+                roman.append(symbols[i]);
+            }
+        }
+        return roman.toString();
     }
-
 }
